74fe0bf14356b2c9cc651ce2f6790ce32435a0e1
JolieYang_Mini <jolie_yang@yeah.net>
2016-08-30 11:10:52 +0800

    (*)UITextView统计字数

--- Out/Out/InputMoodViewController.m
+++ Out/Out/InputMoodViewController.m
@@ -6,6 +6,11 @@
 //  Copyright © 2016年 Jolie_Yang. All rights reserved.
 //
 
+// 问题列表:
+// ?1.预输入时打印textView s s s。 字符为ssss时，打印长度为4，而微博是按2来算。
+// ?2.去除空格失败
+// ?3. 主页面进入该页面是会有点卡顿
+
 #import "InputMoodViewController.h"
 
 #define YLog(formatString, ...) NSLog((@"%s" formatString), __PRETTY_FUNCTION__, ##__VA_ARGS__);
@@ -21,15 +26,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.navigationItem.title = @"InputMood";
     self.inputTextView.delegate = self;
-    [self.inputTextView becomeFirstResponder];
+    self.navigationItem.title = @"InputMood";
     // 设置导航栏右边按钮
     UIBarButtonItem *checkItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"nav_check"] style:UIBarButtonItemStylePlain target:self action:@selector(didEndEdit)];
     self.navigationItem.rightBarButtonItem = checkItem;
     // 设置导航栏返回(左边)按钮
     UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"nav_back"] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
     self.navigationItem.leftBarButtonItem = backItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.inputTextView becomeFirstResponder];
 }
 
 - (void)back {
@@ -42,49 +51,34 @@
 }
 
 #pragma mark UITextViewDelegate
+- (void)textViewDidChange:(UITextView *)textView {
+    UITextRange *selectedRange = [textView markedTextRange];
+    NSString *newText = [textView textInRange:selectedRange];// print: "S s"
+    // ?2. 去除空格失败
+    NSString *failedStrip = [newText stringByReplacingOccurrencesOfString:@" " withString:@""];// print "S s"
+    NSLog(@"failedStrip:%@", failedStrip);
+    NSString *textStr = newText;
+    NSString *stripSpaceStr = [textStr stringByReplacingOccurrencesOfString:@" " withString:@""];
+    self.textLengthLB.text = [NSString stringWithFormat:@"%lu/100", (unsigned long)textView.text.length - newText.length + stripSpaceStr.length];
+}
+
 // 键盘弹出,进入该回调1.
 - (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
 //    YLog();
     return YES;
 }
-// 2.
-- (void)textViewDidBeginEditing:(UITextView *)textView {
-    //    YLog();
-    
-}
-- (BOOL)textViewShouldEndEditing:(UITextView *)textView {
-    
-    YLog();
-    return YES;
-}
-
-
-- (void)textViewDidEndEditing:(UITextView *)textView {
-    YLog();
-    
-}
 
 // 点击键盘上的
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
-    UITextRange *selectedRange = [textView markedTextRange];
-    NSString *newText = [textView textInRange:selectedRange];
-    NSLog(@"text:%@,%@,%@,lenght:%lu", textView.text, self.inputTextView.text, newText, newText.length);
     
     return YES;
 }
 
 // 输入字符
 - (void)textViewDidChangeSelection:(UITextView *)textView {
-//    YLog(@"changeSelection:%@", textView.text);
+    YLog(@"changeSelection:%@", textView.text);
 }
-// 输入字符
-- (void)textViewDidChange:(UITextView *)textView {
-    UITextRange *selectedRange = [textView markedTextRange];
-    NSString *newText = [textView textInRange:selectedRange];
-    NSLog(@"textDidChange:%@,%@,%@,%lu", textView.text, self.inputTextView.text, newText, newText.length);
-    
-    self.textLengthLB.text = [NSString stringWithFormat:@"%lu/100", (unsigned long)newText.length];
-}
+
 
 - (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange {
     YLog();
